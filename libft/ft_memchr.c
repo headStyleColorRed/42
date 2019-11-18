0b82fb7425523b8aa4ff2708ef91e87e8208b364
Rodrigo Labrador serrano <rlabrado@c3r6s3.42madrid.com>
2019-11-18 15:55:00 +0100

    falta corregir un poco el split

---
+++ libft/ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlabrado <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/12 12:34:55 by rlabrado          #+#    #+#             */
+/*   Updated: 2019/11/12 12:35:46 by rlabrado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*ptr;
+	unsigned char	c2;
+	int				i;
+
+	i = 0;
+	c2 = (unsigned char)c;
+	ptr = (unsigned char *)s;
+	while (n--)
+	{
+		if (ptr[i] == c2)
+			return (ptr + i);
+		i++;
+	}
+	return (NULL);
+}
---
+++ libft/ft_strtrim.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlabrado <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/12 12:59:25 by rlabrado          #+#    #+#             */
+/*   Updated: 2019/11/12 13:00:42 by rlabrado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	*ft_strncpy(char *dest, char const *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	if (i < n && src[i] == '\0')
+	{
+		while (dest[i] != '\0')
+		{
+			dest[i] = '\0';
+			++i;
+		}
+	}
+	return (dest);
+}
+
+char		*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	len;
+	char	*trimed;
+
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]) != NULL)
+		start++;
+	len = ft_strlen(&s1[start]);
+	if (len != 0)
+		while (s1[start + len - 1]
+				&& ft_strchr(set, s1[start + len - 1]) != NULL)
+			len--;
+	if ((trimed = (char*)malloc(sizeof(char) * (len + 1))) == NULL)
+		return (NULL);
+	trimed = ft_strncpy(trimed, &s1[start], len);
+	trimed[len] = '\0';
+	return (trimed);
+}
